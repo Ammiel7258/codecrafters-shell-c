d7a1286c0e0c4ad222ad82d0e1ded4e90e9fe337
Ammiel <ammiel.maynard@gmail.com>
2026-01-30 13:44:17 -0500

    pass 1st stage

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+  char command[1024];
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+  fgets(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = '0';
+  printf("%s: command not found", command);
 
   return 0;
 }
